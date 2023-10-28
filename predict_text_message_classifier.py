a952f0f52307a18a2c2e87b0cd9c8dd91e23c35b
Aarsh <aarshps@gmail.com>
2023-10-29 03:13:25 +0530

    Intermiate commit

--- predict_text_message_classifier.py
+++ predict_text_message_classifier.py
@@ -1,10 +1,14 @@
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the trained classifier
-classifier = pickle.load(open('text_message_classifier.pkl', 'rb'))
+classifier = pickle.load(open('model.pkl', 'rb'))
 
 # Predict the label of a new text message
 text_message = 'Dear HDFC Bank customer, your account has been credited with Rs.10,000.'
+
+vectorizer = TfidfVectorizer()
+
 text_message_tfidf = vectorizer.transform([text_message])
 label = classifier.predict(text_message_tfidf)
 
--- train_text_message_classifier.py
+++ train_text_message_classifier.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+import pickle
 import numpy as np
 
 # Load the text message dataset
@@ -23,11 +24,16 @@ classifier = LogisticRegression()
 # Train the classifier on the training data
 classifier.fit(X_train, y_train)
 
+# Save the trained classifier
+with open('model.pkl', 'wb') as f:
+    pickle.dump(classifier, f)
+
+# Load the trained classifier
+with open('model.pkl', 'rb') as f:
+    classifier = pickle.load(f)
+
 # Evaluate the classifier on the testing data
 y_pred = classifier.predict(X_test)
 accuracy = np.mean(y_pred == y_test)
 
 print('Accuracy:', accuracy)
-
-# Save the trained classifier
-classifier.save_model('text_message_classifier.pkl')
