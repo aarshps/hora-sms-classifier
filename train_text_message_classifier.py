f34400f9bc1919a046dc40d68e833e0a780e406f
Aarsh <aarshps@gmail.com>
2023-10-29 02:42:12 +0530

    Updated text messages csv
    Update NaN issue in CSV

--- train_text_message_classifier.py
+++ train_text_message_classifier.py
@@ -2,28 +2,29 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 # Load the text message dataset
 data = pd.read_csv('text_messages.csv')
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.25)
+# Remove the NaN values from the data
+data = data.dropna()
 
-# Create a TF-IDF vectorizer
+# Transform the data into TF-IDF vectors
 vectorizer = TfidfVectorizer()
+X_tfidf = vectorizer.fit_transform(data['text'])
 
-# Transform the training and testing data into TF-IDF vectors
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_tfidf, data['label'], test_size=0.25)
 
 # Create a logistic regression classifier
 classifier = LogisticRegression()
 
 # Train the classifier on the training data
-classifier.fit(X_train_tfidf, y_train)
+classifier.fit(X_train, y_train)
 
 # Evaluate the classifier on the testing data
-y_pred = classifier.predict(X_test_tfidf)
+y_pred = classifier.predict(X_test)
 accuracy = np.mean(y_pred == y_test)
 
 print('Accuracy:', accuracy)
